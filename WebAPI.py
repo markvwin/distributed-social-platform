dea69c2450ac871b5e69c378e03902ee8f23bedb
Mark Nguyen <markvn@uci.edu>
2023-03-10 19:57:34 -0800

    Completed tests for testing WebAPI

--- WebAPI.py
+++ WebAPI.py
@@ -1,28 +1,72 @@
+"""
+Mark Nguyen
+84257566
+markvn@uci.edu
+"""
+
 from abc import ABC, abstractmethod
-import urllib, json
-from urllib import request, error
+import urllib
+import json
+from urllib import request
+from urllib import error
+import http.client
+
+
+class Error401(Exception):
+    """Error401 Exception caused by Invalid Key"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
 
 
 class Error403(Exception):
-    pass
+    """Error403 Exception caused by Invalid Key"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
 
 
 class Error404(Exception):
-    pass
+    """Error404 Exception caused by Invalid URL"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
 
 
 class Error503(Exception):
-    pass
+    """Error503 Exception caused if a server is down"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
+
+
+class LossConnectionError(Exception):
+    """Exception caused if network disconnects during request or before"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
+
+
+class InvalidURLError(Exception):
+    """Exception caused by an InvalidURL"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
+
+
+class InvalidDataFormatError(Exception):
+    """Exception caused if receiving an invalid data format"""
+    def __init__(self, *args: object):
+        super().__init__(args)
+        self.message = args[0]
 
 
 class WebAPI(ABC):
-
+    """Abstract class that API modules inherit from"""
     def __init__(self):
         pass
-        # self.api_key = None
 
     def _download_url(self, url: str) -> dict:
-        # TODO: Implement web api request code in a way that supports ALL types of web APIs
 
         response = None
         r_obj = None
@@ -32,29 +76,38 @@ class WebAPI(ABC):
             json_results = response.read()
             r_obj = json.loads(json_results)
 
-        except urllib.error.HTTPError as He:
+        except urllib.error.HTTPError as h_e:
             print('Failed to download contents of URL')
-            print('Status code: {}'.format(He.code))
-            if He.code == 403:
-                raise Error403('Invalid Access Key', He)
-            elif He.code == 404:
+            print(f'Status code: {h_e.code}')
+            if h_e.code == 401:
+                raise Error401('Invalid Access Key', h_e) from h_e
+            if h_e.code == 403:
+                raise Error403('Invalid Access', h_e) from h_e
+            if h_e.code == 404:
                 raise Error404(
-                    'The page you are looking for does not exist', He)
-            elif He.code == 503:
+                    'The page you are looking for does not exist', h_e) from\
+                    h_e
+            if h_e.code == 503:
                 raise Error503(
                     'Unavailable server. The server is not ready to '
-                    'handle your request', He)
+                    'handle your request', h_e) from h_e
 
-        except urllib.error.URLError as Ue:
+        except urllib.error.URLError as u_e:
             print('Failed to download contents of URL')
-            print('Reason: {}'.format(Ue.reason))
+            print(f'Reason: {u_e.reason}')
+            raise LossConnectionError('Connection Error', u_e) from u_e
 
-        except json.JSONDecodeError as Je:
-            print('JSONDecodeError: Invalid data format')
-
-        except ValueError as Ve:
+        except http.client.InvalidURL as i_u:
             print('Failed to download contents of URL')
-            print('ValueError: Invalid URL')
+            raise InvalidURLError('Invalid URL', i_u) from i_u
+
+        except json.JSONDecodeError as d_e:
+            raise InvalidDataFormatError('JSONDecodeError: Invalid data '
+                                         'format', d_e) from d_e
+
+        except ValueError as v_e:
+            print('Failed to download contents of URL')
+            raise InvalidURLError('Invalid URL', v_e) from v_e
 
         finally:
             if response is not None:
@@ -69,11 +122,10 @@ class WebAPI(ABC):
         """
         self.api_key = apikey
 
-
     @abstractmethod
     def load_data(self):
-        pass
+        """Requires child classes to have a load_data method"""
 
     @abstractmethod
     def transclude(self, message: str) -> str:
-        pass
+        """Requires child classes to have a transclude method"""
