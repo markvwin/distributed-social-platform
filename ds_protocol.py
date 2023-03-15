e489f43a8b1e5db633d1843ad6c8ce458662825d
Mark Nguyen <markvn@uci.edu>
2023-03-14 18:44:43 -0700

    created extract_json_dms() function in ds_protocol.py that extracts responses responsible for showing messages to the User.

--- ds_protocol.py
+++ ds_protocol.py
@@ -6,10 +6,11 @@ import json
 from collections import namedtuple
 
 """ Namedtuple to hold the values retrieved from json messages. """
-DataTuple = namedtuple('TokenTuple', ['type', 'message', 'token'])
+DATA_TUPLE = namedtuple('TokenTuple', ['type', 'message', 'token'])
+DATA_TUPLE1 = namedtuple('MessagesTuple', ['type', 'messages'])
 
 
-def extract_json(json_msg: str) -> DataTuple:
+def extract_json(json_msg: str) -> DATA_TUPLE:
     """
     Call the json.loads function on a json string and convert it to a
     DataTuple object
@@ -21,8 +22,22 @@ def extract_json(json_msg: str) -> DataTuple:
         message = response['message']
         if 'token' in response:
             token = response['token']
-            return DataTuple(resp_type, message, token)
+            return DATA_TUPLE(resp_type, message, token)
         else:
-            return DataTuple(resp_type, message, '')
+            return DATA_TUPLE(resp_type, message, '')
+    except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+
+
+def extract_json_dms(json_msg: str) -> DATA_TUPLE1:
+    try:
+        json_obj = json.loads(json_msg)
+        response = json_obj['response']
+        resp_type = response['type']
+        messages = response['messages']
+        # Message is a list of dictionaries with "message", "from", and
+        # "timestamp" as keys
+        return DATA_TUPLE1(resp_type, messages)
+
     except json.JSONDecodeError:
         print("Json cannot be decoded.")
